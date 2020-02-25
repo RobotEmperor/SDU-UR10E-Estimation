6ceb2652d37105360463cd5f7343cb88ac92e2b4
robotemperor <kimpd1224@gmail.com>
2020-02-25 15:16:33 +0100

    modified

--- ur10e_force_torque_sensor_test/src/ur10e_force_torque_sensor_test.cpp
+++ ur10e_force_torque_sensor_test/src/ur10e_force_torque_sensor_test.cpp
@@ -5,9 +5,35 @@
  *      Author: yik
  */
 #include "ur10e_force_torque_sensor_test/ur10e_force_torque_sensor_test.h"
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
 using namespace ur_rtde;
 volatile sig_atomic_t stop;
+double time_count = 0.0;
+
+ofstream out("test.csv");
+
+string data_line;
+
+
+
+
+void timer_handler (int signum)
+{
+//  static int count = 0;
+//  printf("timer expired %d timers\n", ++count);
+  time_count += 0.002;
+  printf("time_count = %f \n",time_count);
+
+  data_line = to_string(time_count);
+
+  out<<data_line<<endl;
+}
+
 
 void inthand(int signum) {
   stop = 1;
@@ -104,39 +130,39 @@ int main (int argc, char **argv)
 
   joint_vector.resize(6);
 
-//  RTDEReceiveInterface rtde_receive("192.168.1.129");
-//  std::vector<double> joint_positions        = rtde_receive.getActualQ();
-//  std::vector<double> force_data             = rtde_receive.getActualTCPForce();
-//  std::vector<double> tool_linear_acc_data   = rtde_receive.getActualToolAccelerometer();
-//  std::vector<double> tcp_pose_data         = rtde_receive.getActualTCPPose();
-//  std::vector<double> tcp_speed_data         = rtde_receive.getActualTCPSpeed();
+  //  RTDEReceiveInterface rtde_receive("192.168.1.129");
+  //  std::vector<double> joint_positions        = rtde_receive.getActualQ();
+  //  std::vector<double> force_data             = rtde_receive.getActualTCPForce();
+  //  std::vector<double> tool_linear_acc_data   = rtde_receive.getActualToolAccelerometer();
+  //  std::vector<double> tcp_pose_data         = rtde_receive.getActualTCPPose();
+  //  std::vector<double> tcp_speed_data         = rtde_receive.getActualTCPSpeed();
+  //
+  //
+  //  tool_linear_acc_data  = rtde_receive.getActualToolAccelerometer();
+  //  tcp_pose_data = rtde_receive.getActualTCPPose();
+  //  force_data     = rtde_receive.getActualTCPForce();
+
+
+//  usleep(3000000); //
 //
+//  // offset initialize function
+//  for(int num = 1;num < 1001; num ++)
+//  {
+//    ros::spinOnce();
+//    //force_data     = rtde_receive.getActualTCPForce();
+//    //tool_linear_acc_data  = rtde_receive.getActualToolAccelerometer();
+//    for(int var = 0; var < 6; var ++)
+//    {
+//      //raw_force_torque_data(var,0) = force_data[var];
+//    }
+//    //raw_tool_acc_data(0, 0) = tool_linear_acc_data[0];
+//    //raw_tool_acc_data(1, 0) = tool_linear_acc_data[1];
+//    //raw_tool_acc_data(2, 0) = tool_linear_acc_data[2];
 //
-//  tool_linear_acc_data  = rtde_receive.getActualToolAccelerometer();
-//  tcp_pose_data = rtde_receive.getActualTCPPose();
-//  force_data     = rtde_receive.getActualTCPForce();
-
-
-  usleep(3000000); //
-
-  // offset initialize function
-  for(int num = 1;num < 1001; num ++)
-  {
-    ros::spinOnce();
-    //force_data     = rtde_receive.getActualTCPForce();
-    //tool_linear_acc_data  = rtde_receive.getActualToolAccelerometer();
-    for(int var = 0; var < 6; var ++)
-    {
-      //raw_force_torque_data(var,0) = force_data[var];
-    }
-    //raw_tool_acc_data(0, 0) = tool_linear_acc_data[0];
-    //raw_tool_acc_data(1, 0) = tool_linear_acc_data[1];
-    //raw_tool_acc_data(2, 0) = tool_linear_acc_data[2];
-
-    ft_filter -> offset_init(raw_force_torque_data, 1000);
-    tool_estimation -> offset_init(raw_tool_acc_data, 1000);
-    usleep(2000); // 2ms
-  }
+//    ft_filter -> offset_init(raw_force_torque_data, 1000);
+//    tool_estimation -> offset_init(raw_tool_acc_data, 1000);
+//    usleep(2000); // 2ms
+//  }
   //cout <<  tool_estimation->offset_data << "\n\n";
 
   cout <<"\n--------------------------\n";
@@ -150,9 +176,57 @@ int main (int argc, char **argv)
 
   signal(SIGINT, inthand);
 
+  struct sigaction sa;
+  struct itimerval timer;
+
+  /* Install timer_handler as the signal handler for SIGVTALRM. */
+  memset (&sa, 0, sizeof (sa));
+  sa.sa_handler = &timer_handler;
+  sigaction (SIGVTALRM, &sa, NULL);
+
+  /* Configure the timer to expire after 250 msec... */
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 2000;
+
+  /* ... and every 250 msec after that. */
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 2000;
+
+  /* Start a virtual timer. It counts down whenever this process is executing. */
+  setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
+//  string data_line;
+//  string getActualQ;
+//  string getActualTCPPose;
+//  string getActualTCPForce;
+//  string getActualToolAccelerometer;
+//  string getFilteredForce;
+//  string getContactedForceTorque;
+//  getActualTCPPose = ",actual_tcp_pose_x,actual_tcp_pose_y,actual_tcp_pose_z,actual_tcp_pose_r,actual_tcp_pose_p,actual_tcp_pose_y";
+//  getActualTCPForce = ",actual_tcp_force_x,actual_tcp_force_y,actual_tcp_force_z,actual_tcp_force_r,actual_tcp_force_p,actual_tcp_force_y";
+//  getActualToolAccelerometer = ",actual_tcp_acc_x,actual_tcp_acc_y,actual_tcp_acc_z";
+//  getFilteredForce = ",filtered_force_x,filtered_force_y,filtered_force_z,filtered_torque_r,filtered_torque_p,filtered_torque_y";
+//  getContactedForceTorque = ",contacted_force_x,contacted_force_y,contacted_force_z,contacted_torque_r,contacted_torque_p,contacted_torque_y";
+//
+//  ofstream out("test.csv");
+//
+//  data_line = "time"+getActualTCPPose+getActualTCPForce+getActualToolAccelerometer+getFilteredForce+getContactedForceTorque;
+//
+//  out<<data_line<<endl;
+//
+//  getActualTCPPose = "";
+//  getActualTCPForce = "";
+//  getFilteredForce = "";
+//  getContactedForceTorque = "";
+//  getActualToolAccelerometer = "";
+
   while (!stop)
   {
-    usleep(2000); // 2ms
+
+    //usleep(100); // 2ms
+
+
+
     //tool_linear_acc_data  = rtde_receive.getActualToolAccelerometer();
     //force_data     = rtde_receive.getActualTCPForce();
     //joint_positions        = rtde_receive.getActualQ();
@@ -193,29 +267,27 @@ int main (int argc, char **argv)
     //    {
     //      raw_force_torque_data(num,0) = force_data[num];
     //    }
-    tool_acc_data = raw_tool_acc_data - tool_estimation->get_offset_data();
-    ur10e_kinematics->calculate_forward_kinematics(joint_vector);
-    //ur10e_kinematics->calculate_forward_kinematics(joint_positions);
-    tool_acc_data = ur10e_kinematics->get_tf_base_to_tool(tool_acc_data);
-    tool_estimation->set_acc_input_data(tool_acc_data);
-    tool_estimation->set_pose_input_data(tcp_pose);
-    tool_estimation->get_angular_acc();
-
-
-
-    //tool_estimation->get_angular_acc();
-
-    //contacted_force_data = tool_estimation->get_contacted_force(raw_force_torque_data - ft_filter->get_offset_data(), tool_acc_data);
-
-    ft_filter->filter_processing(raw_force_torque_data);
-
-    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"x");
-    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"y");
-    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"z");
-
-    contacted_force_data = tool_estimation->get_contacted_force(ft_filter->get_filtered_data(), tool_acc_data);
-
-
+//    tool_acc_data = raw_tool_acc_data - tool_estimation->get_offset_data();
+//    ur10e_kinematics->calculate_forward_kinematics(joint_vector);
+//    //ur10e_kinematics->calculate_forward_kinematics(joint_positions);
+//    tool_acc_data = ur10e_kinematics->get_tf_base_to_tool(tool_acc_data);
+//    tool_estimation->set_acc_input_data(tool_acc_data);
+//    tool_estimation->set_pose_input_data(tcp_pose);
+//    tool_estimation->get_angular_acc();
+//
+//
+//
+//    //tool_estimation->get_angular_acc();
+//
+//    //contacted_force_data = tool_estimation->get_contacted_force(raw_force_torque_data - ft_filter->get_offset_data(), tool_acc_data);
+//
+//    ft_filter->filter_processing(raw_force_torque_data);
+//
+//    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"x");
+//    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"y");
+//    tool_estimation->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(),"z");
+//
+//    contacted_force_data = tool_estimation->get_contacted_force(ft_filter->get_filtered_data(), tool_acc_data);
 
     //cout << ur10e_kinematics->get_axis_to_euler_angle(tcp_pose_data[3], tcp_pose_data[4], tcp_pose_data[5]) << "\n\n";
     //cout << "--------------------------------" << "\n\n";
@@ -223,37 +295,35 @@ int main (int argc, char **argv)
     //cout << tool_estimation ->get_one_axis_inertia_tensor(ft_filter->get_filtered_data(), "x");
 
 
-    cout << tool_estimation->inertia_of_tool_ << "\n\n";
+    //cout << tool_estimation->inertia_of_tool_ << "\n\n";
+    //
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(0,0));
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(1,0));
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(2,0));
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(3,0));
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(4,0));
+    //    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(5,0));
+    //
+    //
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(0,0));
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(1,0));
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(2,0));
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(3,0));
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(4,0));
+    //    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(5,0));
+    //
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(0,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(1,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(2,0));
+    //
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(3,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(4,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(5,0));
 
 
-
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(0,0));
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(1,0));
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(2,0));
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(3,0));
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(4,0));
-    filtered_force_torque_data_msg.data.push_back(raw_force_torque_data(5,0));
-
-
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(0,0));
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(1,0));
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(2,0));
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(3,0));
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(4,0));
-    filtered_force_torque_data_msg.data.push_back(ft_filter->get_filtered_data()(5,0));
-
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(0,0));
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(1,0));
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(2,0));
-
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(3,0));
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(4,0));
-    filtered_force_torque_data_msg.data.push_back(contacted_force_data(5,0));
-
-
-//    filtered_force_torque_data_msg.data.push_back(contacted_force_data(0,0));
-//    filtered_force_torque_data_msg.data.push_back(contacted_force_data(1,0));
-//    filtered_force_torque_data_msg.data.push_back(contacted_force_data(2,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(0,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(1,0));
+    //    filtered_force_torque_data_msg.data.push_back(contacted_force_data(2,0));
 
     //filtered_force_torque_data_msg.data.push_back(tool_linear_acc_data[0]);
     //filtered_force_torque_data_msg.data.push_back(tool_linear_acc_data[1]);
@@ -281,15 +351,32 @@ int main (int argc, char **argv)
     // transformation check
 
 
-    filtered_force_torque_data_pub.publish(filtered_force_torque_data_msg);
-    filtered_force_torque_data_msg.data.clear();
+    // filtered_force_torque_data_pub.publish(filtered_force_torque_data_msg);
+    // filtered_force_torque_data_msg.data.clear();
 
-    ros::spinOnce();
+    //    for(int num = 0; num<6; num++)
+    //    {
+    //      getActualTCPPose += ","+to_string(tcp_pose(num,0));
+    //      getActualTCPForce += ","+to_string(raw_force_torque_data(num,0));
+    //      getFilteredForce += ","+to_string(ft_filter->get_filtered_data()(num,0));
+    //      getContactedForceTorque += ","+to_string(contacted_force_data(num,0));
+    //    }
+    //    for(int num = 0; num<3; num++)
+    //    {
+    //      getActualToolAccelerometer += ","+to_string(tool_acc_data(num,0));
+    //    }
+
+
+
+
+    //ros::spinOnce();
   }
 
-  usleep(10000);
-  cout << "complete" << "\n\n";
 
+
+  usleep(10000);
+  out.close();
+  cout << "complete and save" << "\n\n";
   printf("exiting safely\n");
   system("pause");
 
